4053b0dfd4b4de0f86d9f4e45c202221f3aa00f1
Pedro Holanda <pedroholanda@gmail.com>
2026-02-23 11:16:54 +0100

    Fix autoloader

--- src/storage/ducklake_autoload_helper.cpp
+++ src/storage/ducklake_autoload_helper.cpp
@@ -17,32 +17,15 @@ static string LookupExtensionForPattern(const string &pattern) {
 }
 
 void DuckLakeInitializer::CheckAndAutoloadedRequiredExtension(const string &pattern) {
-	// This functions will:
-	//	1. Check if a known extension pattern matches the start of the data_path
-	//	2. If so, either load the required extension or throw a relevant error message
-
-	// FIXME: This function is currently a copy of the logic at FileSystem::GlobFiles in duckdb/duckdb (version 1.3.0)
-	// repository Proper solution would be offer this functionality as part of DuckDB C++ API, so this file can be
-	// simplified reducing the risk of misalignment between the two codebases
-
-	string required_extension = LookupExtensionForPattern(pattern);
-	if (!required_extension.empty() && !context.db->ExtensionIsLoaded(required_extension)) {
-		if (!ExtensionHelper::CanAutoloadExtension(required_extension) ||
-		    !Settings::Get<AutoloadKnownExtensionsSetting>(context)) {
-			auto error_message =
-			    "Data path " + pattern + " requires the extension " + required_extension + " to be loaded";
-			error_message =
-			    ExtensionHelper::AddExtensionInstallHintToErrorMsg(context, error_message, required_extension);
-			throw MissingExtensionException(error_message);
-		}
-		// an extension is required to read this file, but it is not loaded - try to load it
-		ExtensionHelper::AutoLoadExtension(context, required_extension);
-		// success! glob again
-		// check the extension is loaded just in case to prevent an infinite loop here
-		if (!context.db->ExtensionIsLoaded(required_extension)) {
-			throw InternalException("Extension load \"%s\" did not throw but somehow the extension was not loaded",
-			                        required_extension);
-		}
+	if (pattern.empty()) {
+		return;
+	}
+	const string required_extension = LookupExtensionForPattern(pattern);
+	if (!required_extension.empty() && !ExtensionHelper::TryAutoLoadExtension(*context.db, required_extension)) {
+		auto error_message =
+		    "Ducklake data path " + pattern + " requires the extension " + required_extension + " to be loaded";
+		error_message = ExtensionHelper::AddExtensionInstallHintToErrorMsg(context, error_message, required_extension);
+		throw MissingExtensionException(error_message);
 	}
 }
 
