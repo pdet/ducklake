30fcf5ff3546ce1ebd311b1fed5c4370343a6009
Pedro Holanda <pedroholanda@gmail.com>
2026-09-21 14:16:39 +0200

    cleanup

--- src/common/ducklake_util.cpp
+++ src/common/ducklake_util.cpp
@@ -292,72 +292,7 @@ string ToByteaHexLiteral(const string &raw_bytes) {
 	return "'\\x" + hex + "'";
 }
 
-static string ValueToSQLInternal(DuckLakeMetadataManager &metadata_manager, ClientContext &context, const Value &val);
-
-//! Splits the opening brace out of {UPPERCASE} patterns in a single quoted literal
-static string SplitPlaceholders(const string &literal) {
-	string result;
-	for (idx_t i = 0; i < literal.size(); i++) {
-		if (literal[i] == '{') {
-			idx_t end = i + 1;
-			while (end < literal.size() && ((literal[end] >= 'A' && literal[end] <= 'Z') || literal[end] == '_')) {
-				end++;
-			}
-			if (end > i + 1 && end < literal.size() && literal[end] == '}') {
-				result += "' || chr(123) || '";
-				continue;
-			}
-		}
-		result += literal[i];
-	}
-	return result;
-}
-
-//! Keeps {SNAPSHOT_ID} style placeholders inside data literals from being substituted by the commit batch
-static string NeutralizePlaceholders(const string &sql) {
-	string result;
-	idx_t i = 0;
-	while (i < sql.size()) {
-		if (sql[i] != '\'') {
-			result += sql[i++];
-			continue;
-		}
-		// find the closing quote, '' is an escaped quote
-		idx_t end = i + 1;
-		while (end < sql.size()) {
-			if (sql[end] == '\'') {
-				if (end + 1 < sql.size() && sql[end + 1] == '\'') {
-					end += 2;
-					continue;
-				}
-				break;
-			}
-			end++;
-		}
-		if (end >= sql.size()) {
-			// unterminated literal, leave the rest untouched
-			result += sql.substr(i);
-			break;
-		}
-		auto literal = sql.substr(i, end - i + 1);
-		// a literal followed by a single colon is a struct key
-		idx_t after = end + 1;
-		while (after < sql.size() && sql[after] == ' ') {
-			after++;
-		}
-		bool is_struct_key =
-		    after < sql.size() && sql[after] == ':' && (after + 1 >= sql.size() || sql[after + 1] != ':');
-		result += is_struct_key ? literal : SplitPlaceholders(literal);
-		i = end + 1;
-	}
-	return result;
-}
-
 string DuckLakeUtil::ValueToSQL(DuckLakeMetadataManager &metadata_manager, ClientContext &context, const Value &val) {
-	return NeutralizePlaceholders(ValueToSQLInternal(metadata_manager, context, val));
-}
-
-static string ValueToSQLInternal(DuckLakeMetadataManager &metadata_manager, ClientContext &context, const Value &val) {
 	// FIXME: this should be upstreamed
 	if (val.IsNull()) {
 		return val.ToString();
@@ -365,7 +300,7 @@ static string ValueToSQLInternal(DuckLakeMetadataManager &metadata_manager, Clie
 	if (val.type().HasAlias()) {
 		// extension type: cast to string
 		auto str_val = val.CastAs(context, LogicalType::VARCHAR);
-		return ValueToSQLInternal(metadata_manager, context, str_val);
+		return ValueToSQL(metadata_manager, context, str_val);
 	}
 	string result;
 	switch (val.type().id()) {
