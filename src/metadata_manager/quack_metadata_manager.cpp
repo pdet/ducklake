3f2bb1d6c949f80fec78fded9ba323babe13b8fa
Maia <maia@duckdblabs.com>
2026-09-23 15:23:59 +0200

    Use single QueryResult type

--- src/metadata_manager/quack_metadata_manager.cpp
+++ src/metadata_manager/quack_metadata_manager.cpp
@@ -2,7 +2,6 @@
 #include "common/ducklake_util.hpp"
 #include "duckdb/catalog/catalog.hpp"
 #include "duckdb/main/connection.hpp"
-#include "duckdb/main/materialized_query_result.hpp"
 #include "storage/ducklake_catalog.hpp"
 #include "storage/ducklake_staged_commit.hpp"
 #include "storage/ducklake_transaction.hpp"
